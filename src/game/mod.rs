9149b3842b7a3f4125670cd3a973c8e6af32a893
InnocentusLime <innocentuslime.help@gmail.com>
2024-07-24 21:32:41 +0300

    Do not remove target component

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -46,7 +46,6 @@ impl Plugin for GamePlugin {
 
         /* Minion systems */
         app.add_systems(Update, cleanup_minion_state)
-            .add_systems(Update, cleanup_minion_target)
             .add_systems(Update, update_minion_state)
             .add_systems(Update, minion_walk.after(update_minion_state))
             .add_systems(Update, walk_target_update.after(update_minion_state))
